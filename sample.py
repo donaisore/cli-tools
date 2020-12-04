b1c23e3860e7e730586982ebc0b1a16b9c0945f2
donaisore <r.h.kobe0706@gmail.com>
2020-12-04 20:43:07 +0900

    delete git branches.

--- sample.py
+++ sample.py
@@ -1,12 +1,29 @@
+import fire
+import sys
 import inquirer
 import subprocess
-res = subprocess.call('git branch')
-print(res)
 
-# questions = [inquirer.Checkbox(
-#     'interests',
-#     message="What are you interested in?",
-#     choices=['Computers', 'Books', 'Science', 'Nature', 'Fantasy', 'History'],
-# )]
-# answers = inquirer.prompt(questions)  # returns a dict
-# print(answers['interests']) 
+def delete_git_branches():
+    output = subprocess.check_output(['git', '-P', 'branch'])
+    branch_list = output.decode().replace(' ', '').replace('*', '').split('\n')[:-1]
+
+    questions = [inquirer.Checkbox(
+        'delete_branch_list',
+        message="削除するブランチを選択",
+        choices=branch_list
+    )]
+
+    answers = inquirer.prompt(questions)
+
+    if not answers:
+        sys.exit()
+
+    delete_branch_list = answers.get('delete_branch_list')
+
+    if delete_branch_list:
+        subprocess.call(['git', 'branch', '-D'] + delete_branch_list)
+    print('選択されたブランチはありませんでした。')
+
+
+if __name__ == '__main__':
+    fire.Fire(delete_git_branches)
